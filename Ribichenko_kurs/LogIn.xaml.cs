3ac194e001f35c47fd875abdc2b97a1c43a0b997
Ribichenko <99405938+Ribichenko@users.noreply.github.com>
2022-05-16 09:39:52 +0300

    kruto

---
+++ Ribichenko_kurs/LogIn.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Ribichenko_kurs
+{
+    /// <summary>
+    /// Логика взаимодействия для LogIn.xaml
+    /// </summary>
+    public partial class LogIn : Window
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void Button_Sver_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void login_button_Click(object sender, RoutedEventArgs e)
+        {
+            LogIn min2 = new LogIn();
+            min2.Show();
+            Close();
+        }
+        private void reg_link_Click(object sender, RoutedEventArgs e)
+        {
+
+            MainWindow min1 = new MainWindow();
+            min1.Show();
+            Close();
+        }
+
+        private void sbros_link_Click(object sender, RoutedEventArgs e)
+        {
+            Sbros min3 = new Sbros();
+            min3.Show();
+            Close();
+        }
+    }
+}
---
+++ Ribichenko_kurs/Sbros.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Ribichenko_kurs
+{
+    /// <summary>
+    /// Логика взаимодействия для Sbros.xaml
+    /// </summary>
+    public partial class Sbros : Window
+    {
+        public Sbros()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void Button_Sver_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void reg_link_Click(object sender, RoutedEventArgs e)
+        {
+            LogIn min2 = new LogIn();
+            min2.Show();
+            Close();
+        }
+
+        private void sbros_button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
